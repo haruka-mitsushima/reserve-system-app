b65f8b93c9d111c67e193aa96c5cef1f35f49274
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-04-13 15:00:23 +0900

    Headerコンポーネントの追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Top from "./components/Top";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path="/" element={<Top />}></Route>
+        <Route path="/" element={<Top />} />
       </Routes>
     </Router>
   );
