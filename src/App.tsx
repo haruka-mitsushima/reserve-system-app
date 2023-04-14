246cc6bede80ed662b54e74158de73406c481e91
Inamura Tamaki <tinamura0159@gmail.com>
2023-04-14 12:58:42 +0900

    selectタグ追加 ＠Reserve.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Top from "./components/Top";
-import Header from "./components/Header";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Top from './components/Top';
+import Header from './components/Header';
+import Reserve from './components/Reserve';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Top />} />
+        <Route path="/reserve" element={<Reserve />} />
       </Routes>
     </Router>
   );
