eb9dc08021f490fee850deab6e39aecd5ec6da7d
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-04-18 14:51:42 +0900

    Top.test修正

--- src/tests/Top.test.tsx
+++ src/tests/Top.test.tsx
@@ -36,8 +36,8 @@ const handlers = [
   }),
 ];
 
-const category_PC = 'PC';
-const pc = [
+const category = 'PC';
+const items = [
   {
     name: 'Surface - 01',
     category: 'PC',
@@ -52,32 +52,6 @@ const pc = [
     name: 'ThinkPad - 02',
     category: 'PC',
     id: 3,
-  },
-];
-const category_room = '会議室';
-const room = [
-  {
-    name: '大会議室',
-    category: '会議室',
-    id: 4,
-  },
-  {
-    name: '中会議室',
-    category: '会議室',
-    id: 5,
-  },
-];
-const category_car = '社用車';
-const car = [
-  {
-    name: 'トヨタ・ハイエースバン',
-    category: '社用車',
-    id: 23,
-  },
-  {
-    name: '日産・NV350キャラバン',
-    category: '社用車',
-    id: 24,
   },
 ];
 
@@ -99,15 +73,13 @@ afterAll(() => {
 describe('Top', () => {
   it('1 Should render components correctly in Top component', async () => {
     render(<Top />);
-    // screen.debug();
     expect(screen.getByText('会議室')).toBeInTheDocument();
     expect(screen.getByText('社用車')).toBeInTheDocument();
     expect(screen.getByText('PC')).toBeInTheDocument();
   });
 
-  it('2 Should render components correctly in Items component when props is PC', async () => {
-    render(<Items category={category_PC} items={pc} />);
-    // screen.debug();
+  it('2 Should render components correctly in Items component', async () => {
+    render(<Items category={category} items={items} />);
     expect(screen.getAllByTestId('card')[0]).toBeTruthy();
     expect(screen.getAllByTestId('card')[1]).toBeTruthy();
     expect(screen.getAllByTestId('card')[2]).toBeTruthy();
@@ -119,30 +91,8 @@ describe('Top', () => {
     expect(screen.getByText('ThinkPad - 02')).toBeInTheDocument();
   });
 
-  it('3 Should render components correctly in Items component when props is room', async () => {
-    render(<Items category={category_room} items={room} />);
-    // screen.debug();
-    expect(screen.getAllByTestId('card')[0]).toBeTruthy();
-    expect(screen.getAllByTestId('card')[1]).toBeTruthy();
-    expect(screen.getByAltText('大会議室')).toBeTruthy();
-    expect(screen.getByAltText('中会議室')).toBeTruthy();
-    expect(screen.getByText('大会議室')).toBeInTheDocument();
-    expect(screen.getByText('中会議室')).toBeInTheDocument();
-  });
-
-  it('4 Should render components correctly in Items component when props is car', async () => {
-    render(<Items category={category_car} items={car} />);
-    // screen.debug();
-    expect(screen.getAllByTestId('card')[0]).toBeTruthy();
-    expect(screen.getAllByTestId('card')[1]).toBeTruthy();
-    expect(screen.getByAltText('トヨタ・ハイエースバン')).toBeTruthy();
-    expect(screen.getByAltText('日産・NV350キャラバン')).toBeTruthy();
-    expect(screen.getByText('トヨタ・ハイエースバン')).toBeInTheDocument();
-    expect(screen.getByText('日産・NV350キャラバン')).toBeInTheDocument();
-  });
-
-  it('5 Should navigate when click card', async () => {
-    render(<Items category={category_PC} items={pc} />);
+  it('3 Should navigate when click card', async () => {
+    render(<Items category={category} items={items} />);
     await userEvent.click(screen.getAllByTestId('card')[0]);
     expect(mockNavigate).toBeCalledWith('/*');
     expect(mockNavigate).toHaveBeenCalledTimes(1);
