205f8b6ef9f41c1eb9cb57a1cf843f45cc7bc9d7
Inamura Tamaki <tinamura0159@gmail.com>
2023-04-27 09:53:41 +0900

    コメント追加

--- src/tests/selectOption.ts
+++ src/tests/selectOption.ts
@@ -16,12 +16,14 @@ export async function selectOption(
   expect(button).toHaveTextContent(expectedTextContent);
 }
 
+// 日付のinput入力関数
 export async function selectDate(selectTestId: string, selectDate: string) {
   const inputDate = screen.getByTestId(selectTestId) as HTMLInputElement;
   fireEvent.change(inputDate, { target: { value: selectDate } });
   expect(inputDate.value).toBe(selectDate);
 }
 
+// タイトルのinput入力関数
 export async function inputValue(placeHolder: string, text: string) {
   const inputValue = screen.getByPlaceholderText(placeHolder);
   await userEvent.type(inputValue, text);
